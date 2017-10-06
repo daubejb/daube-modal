34a6604716a138ad6c791b9e0db5c31762cbb576
Jeffrey B. Daube <daubejb@gmail.com>
2017-10-06 12:27:21 -0400

    initial commit, hello github

---
+++ README.md
@@ -0,0 +1,29 @@
+# daube-modal
+A simple, material-influenced, vanilla js custom element.  A simple modal pop-up dialog --- material-influenced, vanilla js custom element
+
+## Installation
+```bash
+npm install --save daube-modal
+```
+
+This custom element requires the [webcomponents-lite polyfill](https://github.com/webcomponents/webcomponentsjs) in order to work on all browsers
+
+Please see [webcomponents.org](https://www.webcomponents.org/polyfills) for more information about Polyfills
+
+Import the custom element by:
+```JavaScript
+<script src="./node_modules/daube-modal/daube-modal-compiled.js"></script>
+```
+
+## Usage
+Include the <daube-modal> custom element in the html page
+```html
+<daube-modal></daube-modal>
+```
+
+## Options
+
+The following options are available within the daube-modal tag:
+
+
+
---
+++ daube-modal.js
@@ -0,0 +1,123 @@
+const daubeModalTemplate = document.createElement('template');
+daubeModalTemplate.innerHTML = `
+  <style>
+
+    :host {
+      display: block;
+      font-size: 1rem;
+      font-family: Helvetica, Verdana, sans-serif;
+      color: rgba(0,0,0,0.87);
+      margin: 0;
+      padding: 0;
+    }
+
+    :host([hidden]) {
+      display: none;
+    }
+
+    #daubemodal {
+      position: fixed;
+      border-radius: 3px;
+      box-shadow:  0px 11px 15px -7px rgba(0, 0, 0, 0.2),
+                    0px 24px 38px 3px rgba(0, 0, 0, 0.14),
+                    0px 9px 46px 8px rgba(0, 0, 0, 0.12);
+      padding: 2rem;
+      background-color: white;
+      margin: auto;
+      width: 16rem;
+      height: 10rem;
+      z-index: 2;
+      top: 0;
+      right: 0;
+      bottom: 0;
+      left: 0;
+    }
+    #entirescreen {
+      width: 100%;
+      height: 100%;
+      z-index: 1;
+      position: fixed;
+      padding: 0;
+      margin: 0;
+      top: 0;
+      left: 0;
+      background-color: rgba(0,0,0,0.2);
+    }
+  </style>
+  <div id="entirescreen" style="display: none;"></div>
+  <div id="daubemodal" style="display: none;">
+    <slot name="question"></slot>
+    <slot name="details"></slot>
+    <slot name="message"></slot>
+    <slot name="negative"></slot>
+    <slot name="positive"></slot>
+  </div>
+`;
+
+if (window.ShadyCSS) {
+  ShadyCSS.prepareTemplate(daubeModalTemplate, 'daube-modal');
+}
+
+class DaubeModal extends HTMLElement {
+  static get observedAttributes() {
+    return ['display'];
+  }
+  get display() {
+    return this.hasAttribute('display');
+  }
+
+  set display(val) {
+    if (val) {
+      this.setAttribute('display', '');
+    } else {
+      this.removeAttribute('display');
+    }
+  }
+  attributeChangedCallback (atrValue, oldValue, newValue) {
+    if (atrValue === 'display') {
+      this.toggleDisplay();
+      console.log('display toggled');
+    }
+  }
+  constructor() {
+    super();
+
+    this.attachShadow({mode: 'open'});
+    this.shadowRoot.appendChild(daubeModalTemplate.content.cloneNode(true));
+  }
+  connectedCallback() {
+    var entirescreen = this.shadowRoot.querySelector("#entirescreen");
+    entirescreen.addEventListener('click', e => {
+      console.log('entire screen clicked');
+      this.removeAttribute('display');
+    })
+    var primBtn = this.querySelector(".primary");
+    primBtn.addEventListener('click', e => {
+      console.log('primary button clicked');
+      this.removeAttribute('display');
+    });
+    var secoBtn = this.querySelector(".secondary");
+    secoBtn.addEventListener('click', e => {
+      console.log('secondary button clicked');
+      this.removeAttribute('display');
+    });
+
+    if (window.ShadyCSS) {
+      ShadyCSS.styleElement(this);
+    }
+  }
+
+  toggleDisplay() {
+    var daubeModal = this.shadowRoot.querySelector('#daubemodal');
+    var entirescreen = this.shadowRoot.querySelector('#entirescreen');
+    if (this.display) {
+      daubeModal.style.display = 'block';
+      entirescreen.style.display = 'block';
+    } else {
+      daubeModal.style.display = 'none';
+      entirescreen.style.display = 'none';
+    }
+  }
+
+} // Class CustomElement
+customElements.define("daube-modal", DaubeModal);
